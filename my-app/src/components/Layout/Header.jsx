a88a6717339edbf6c70e3c6923a98639532f463e
Moiz Ali <moizmughalali@gmail.com>
2025-10-14 19:16:43 +0500

    just changes some few things

--- my-app/src/components/Layout/Header.jsx
+++ my-app/src/components/Layout/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center  h-17 text-green-700'>
+    <div className='flex items-center justify-center  h-17 text-black-700'>
         <h1 className='text-4xl'>Let's Draw</h1>
     </div>
   )
